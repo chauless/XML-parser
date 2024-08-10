b2569c5fd36479f3a13315efdf9af4340004efab
Matvei Morenkov <morenmat@fel.cvut.cz>
2024-08-10 10:58:13 +0200

    use ResponseEntity

--- src/main/java/trixi/smartform/controller/DownloadController.java
+++ src/main/java/trixi/smartform/controller/DownloadController.java
@@ -1,6 +1,8 @@
 package trixi.smartform.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.w3c.dom.Document;
@@ -16,15 +18,14 @@ public class DownloadController {
     private final DataProcessingService dataProcessingService;
 
     @GetMapping("/download")
-    public String downloadAndProcess() {
+    public ResponseEntity<String> downloadAndProcess() {
         try {
             Document xmlFile = fileDownloadService.downloadAndExtractXml();
             dataProcessingService.processAndSaveData(xmlFile);
-
-            return "Download and processing completed.";
+            return ResponseEntity.ok("Download and processing completed.");
         } catch (Exception e) {
-            e.printStackTrace();
-            return "An error occurred during data processing: " + e.getMessage();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("An error occurred during data processing: " + e.getMessage());
         }
     }
 }
